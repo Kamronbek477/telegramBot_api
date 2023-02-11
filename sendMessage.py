f04e78e94838867b0fc95b9009399bbd7d052e00
Jalilov <javohirjaliloff@gmail.com>
2023-02-11 14:43:27 +0500

    markdown bolt italic

--- sendMessage.py
+++ sendMessage.py
@@ -1,9 +1,16 @@
 import os
 import requests
 
-TOKEN = os.environ['TOKEN']
+TOKEN = '5661659754:AAHjcXFhVC9UdI0Q83YBMReNptJXBXFFESs'
+
 
 def sendMessage(chat_id:str, text:str):
     URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+        'chat_id':chat_id, 
+        'text':text,
+        'parse_mode':'MarkdownV2'}
+    response = requests.post(url=URL, data=payload)
+    return response.json()
     
-    
+print(sendMessage(5575549228, "*Hello*  _Hello_"))
